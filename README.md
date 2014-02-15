61a89c9d500ae09833fcde8fa17830b8ef9f6688
crosson.david@gmail.com <crosson.david@gmail.com>
2014-02-15 19:06:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# primes-scalatra-app #
+
+## Build & Run ##
+
+```sh
+$ cd primes-scalatra-app
+$ ./sbt
+> container:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/fr/janalyse/primesui/PrimesServlet.scala
@@ -0,0 +1,17 @@
+package fr.janalyse.primesui
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class PrimesServlet extends PrimesscalatraappStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
