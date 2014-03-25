6351e2385bdfdd1d2bbf432f1d870d209d649012
crosson.david@gmail.com <crosson.david@gmail.com>
2014-03-25 23:10:14 +0100

    using ehcache - fix

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -2,7 +2,7 @@ import fr.janalyse.primesui._
 import org.scalatra._
 import javax.servlet.ServletContext
 
-class ScalatraBootstrap extends LifeCycle with PrimesDBInit {
+class ScalatraBootstrap extends LifeCycle with PrimesDBInit with PrimesCacheInit {
   override def init(context: ServletContext) {
     dbSetup()
     context.mount(new PrimesServlet, "/*")
