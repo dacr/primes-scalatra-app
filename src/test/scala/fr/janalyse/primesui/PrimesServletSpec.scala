efd880f7551043852aa009456978e8c4a147a264
Crosson David <crosson.david@gmail.com>
2015-02-27 10:08:03 +0100

    nop

--- src/test/scala/fr/janalyse/primesui/PrimesServletSpec.scala
+++ src/test/scala/fr/janalyse/primesui/PrimesServletSpec.scala
@@ -3,16 +3,16 @@ package fr.janalyse.primesui
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-/*
-class PrimesServletSpec extends ScalatraSpec { def is =
-  "GET / on PrimesServlet"                     ^
-    "should return status 200"                  ! root200^
-                                                end
 
-  addServlet(classOf[PrimesServlet], "/*")
-
-  def root200 = get("/") {
-    status must_== 200
-  }
-}
-*/
+//class PrimesServletSpec extends ScalatraSpec { def is =
+//  "GET / on PrimesServlet"                     ^
+//    "should return status 200"                  ! root200^
+//                                                end
+//
+//  addServlet(classOf[PrimesServlet], "/*")
+//
+//  def root200 = get("/") {
+//    status must_== 200
+//  }
+//}
+//
