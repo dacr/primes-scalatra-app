0f588bab1ec0bb70dde28424f778be8a78e94144
crosson.david@gmail.com <crosson.david@gmail.com>
2015-03-02 22:22:27 +0100

    dependencies updates

---
+++ RELEASENOTES.md
@@ -0,0 +1,14 @@
+
+# 0.1.1
+ * scalatra 2.3.0
+ * scala 2.11.5
+ * mysql-connector-java 5.1.34
+ * logback 1.1.2
+ * ehcache-core 2.6.10
+ * squeryl 0.9.5-7
+ * c3p0 0.9.5
+ * janalyse-jmx 0.7.1
+ * primes 1.2.1
+ * jetty-webapp 8.1.16.v20140903
+ * javax.servlet.jsp 2.2.0.v201112011158 added to avoid exception with container:start
+ 
--- project/build.scala
+++ project/build.scala
@@ -2,15 +2,13 @@ import sbt._
 import Keys._
 import org.scalatra.sbt._
 import org.scalatra.sbt.PluginKeys._
-//import com.mojolly.scalate.ScalatePlugin._
-//import ScalateKeys._
 
 object PrimesscalatraappBuild extends Build {
   val Organization = "fr.janalyse"
   val Name = "primesui"
-  val Version = "0.1.0"
-  val ScalaVersion = "2.10.3"
-  val ScalatraVersion = "2.2.2"
+  val Version = "0.1.1"
+  val ScalaVersion = "2.11.5"
+  val ScalatraVersion = "2.3.0"
 
   lazy val project = Project (
     "primes-scalatra-app",
@@ -25,41 +23,26 @@ object PrimesscalatraappBuild extends Build {
       resolvers += "JAnalyse Repository" at "http://www.janalyse.fr/repository/",
       libraryDependencies ++= Seq(
         "org.scalatra" %% "scalatra" % ScalatraVersion,
-        //"org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        //"com.typesafe" %% "scalalogging-slf4j" % "1.0.1",
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "org.scalatra" %% "scalatra-scalatest" % "2.2.2" % "test",
-        "fr.janalyse" %% "primes" % "1.0.9",
-        "fr.janalyse" %% "janalyse-jmx" % "0.6.3",
-        "org.squeryl" %% "squeryl" % "0.9.5-6",
-        "com.mchange" % "c3p0" % "0.9.5-pre8",
-        "net.sf.ehcache" % "ehcache-core" % "2.6.8",
+        "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
+        "fr.janalyse" %% "primes" % "1.2.1",
+        "fr.janalyse" %% "janalyse-jmx" % "0.7.1",
+        "org.squeryl" %% "squeryl" % "0.9.5-7",
+        "com.mchange" % "c3p0" % "0.9.5",
+        "net.sf.ehcache" % "ehcache-core" % "2.6.10",
         "javax.transaction" % "jta" % "1.1", // required for ehcache
-        "mysql" % "mysql-connector-java" % "5.1.33",
-        "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
+        "mysql" % "mysql-connector-java" % "5.1.34",
+        "ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.16.v20140903" % "container",
+        "org.eclipse.jetty.orbit" % "javax.servlet.jsp" % "2.2.0.v201112011158" % "container;provided;test",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ).map(
           _.exclude("org.scala-lang", "scala-compiler")
            .exclude("org.scala-lang", "scala-reflect")
-           .exclude("com.typesafe.akka", "akka-actor_2.10")
-	   .exclude("org.scala-lang", "jline")
+           .exclude("com.typesafe.akka", "akka-actor_2.11")
+	         .exclude("org.scala-lang", "jline")
             )
       
-      /*,
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
-        Seq(
-          TemplateConfig(
-            base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
-            Seq(
-              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
-            Some("templates")
-          )
-        )
-      }
-      */
     )
   )
 }
