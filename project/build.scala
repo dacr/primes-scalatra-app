e92422abcda141bc24e5e3be035a346ce2102cfe
Crosson David <david.crosson@orange.com>
2015-09-28 17:10:33 +0200

    primes lib updated to snapshot

--- project/build.scala
+++ project/build.scala
@@ -31,7 +31,7 @@ object PrimesscalatraappBuild extends Build {
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
         "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
-        "fr.janalyse" %% "primes" % "1.2.1",
+        "fr.janalyse" %% "primes" % "1.2.2-SNAPSHOT",
         "fr.janalyse" %% "janalyse-jmx" % "0.7.1",
         "org.squeryl" %% "squeryl" % "0.9.5-7",
         "com.mchange" % "c3p0" % "0.9.5.1",
